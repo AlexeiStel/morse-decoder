f73fc952766e6240a4d69be9e75485cc44fa086e
Aliaksei Stelmakh <alexeistel@gmail.com>
2020-03-09 14:49:24 +0300

    feat: add solution Morze-decoder

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ const MORSE_TABLE = {
     '-..-':   'x',
     '-.--':   'y',
     '--..':   'z',
+    '|||||':  ' ',
     '.----':  '1',
     '..---':  '2',
     '...--':  '3',
@@ -38,9 +39,62 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let arr10 = [];
+    let arr2 = [];
+    let arrMorze =[];
+    let word;
+    let k = 0;
+    
+    for (let i = 0; i < expr.length / 10; i++) {
+        arr10[i] = [];
+    }
+    for (let i = 0; i < expr.length / 10; i++) {
+        arr2[i] = [];
+    }
+    for (let i = 0; i < arr10.length; i++) {
+        for (let j = 0; j < 10; j++) {
+            arr10[i][j] = expr[k]; 
+            k++;
+        }
+    arr.push(arr10[i].join(''));
+    }
+   
+    for (let j = 0; j < arr.length; j++) {
+        for (let i = 0; i < 5; i++) {      
+               arr2[j][0] = arr[j].slice(0, 2);
+               arr2[j][1] = arr[j].slice(2, 4);
+               arr2[j][2] = arr[j].slice(4, 6);
+               arr2[j][3] = arr[j].slice(6, 8);
+               arr2[j][4] = arr[j].slice(8);
+       }
+   }
+   for (let i = 0; i < arr2.length; i++) {
+    for (let j = 0; j < arr2[i].length; j++) {
+        if (arr2[i][j] == 00) {
+            arr2[i][j] = '';
+        } else if (arr2[i][j] == 10) {
+            arr2[i][j] = '.';
+        } else if (arr2[i][j] == 11) {
+            arr2[i][j] = '-';
+        } else if (arr2[i][j] == "**" ) {
+            arr2[i][j] = '|';
+    }
 }
+arrMorze[i] = arr2[i].join('');
+
+}
+    word = arrMorze.map(function (item) {
+    for (let key in MORSE_TABLE) {
+        if (item == key) {
+            return item = MORSE_TABLE[key]; 
+        }
+    }
+});
+return word.join('');
+}
+
 
 module.exports = {
     decode
-}
+};
